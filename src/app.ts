5e68c6368c9f6f1da1e6f6f64cdca966224b57a0
photoby64 <photoby64@naver.com>
2025-01-08 11:51:54 +0900

    learn.react
    - 25.01.08 오전 수업
    - nodemon, rimraf, ts-node 설치
    - Node.js의 fsPromises.readFile() API 함수 사용
    - Express.js의 라우팅 핸들러(entry) 분리
    - Express.js 앱의 미들웨어(Middleware) - greetingMessage.ts
    - 정적 자산 서비스 설정

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+// --------------------------------------------------------------------------
+// TypeScript + Express.js를 활용해 간단한 API 서버 구성
+// --------------------------------------------------------------------------
+//
+// 라우팅은 특정 엔드포인트에 대한 클라이언트 요청에 애플리케이션이 어떻게 응답할지 결정하는 것을 말하며,
+// 이는 URI(또는 경로)와 특정 HTTP 요청 메서드(GET, POST, PUT, PATCH, DELETE 등)입니다.
+// 각 경로에는 하나 이상의 핸들러 함수가 있을 수 있으며, 이 함수는 경로가 일치할 때 실행됩니다.
+//
+// 이미지, 스타일, 스크립트 파일과 같은 정적 파일을 제공하려면 기본 제공되는 미들웨어 함수를 사용합니다.
+// 여러 정적 에셋 디렉토리를 사용하려면 express.static 미들웨어 함수를 여러 번 호출합니다.
+//
+// ✅ Express.js
+// ✅ nodemon, rimraf, ts-node 설치
+// ✅ Node.js의 fsPromises.readFile() API 함수 사용
+// ✅ Express.js의 라우팅 핸들러(entry) 분리
+// ✅ Express.js 앱의 미들웨어(Middleware) - greetingMessage.ts
+// ✅ 정적 자산 서비스 설정
+// --------------------------------------------------------------------------
+import 'dotenv/config';
+import express from 'express';
+import type { Express } from 'express';
+import entryHandler from './handlers/entry';
+import greetingMessage from './middlewares/greetingMessage';
+import { resolve } from 'node:path';
+
+const app: Express = express();
+
+// for Windows Users
+const HOSTNAME = 'localhost';
+// const HOSTNAME = process.env.HOSTNAME ?? 'localhost';
+const PORT = Number(process.env.PORT) ?? 4000;
+const MESSAGE = `웹 서버 구동 : http://${HOSTNAME}:${PORT}`;
+
+
+/* Middleware --------------------------------------------------------------- */
+
+app.use(greetingMessage);
+// app.use(express.static('public'));
+app.use(express.static(resolve(__dirname, '../public')));
+
+/* Routing ------------------------------------------------------------------ */
+
+app.get('/', entryHandler);
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(MESSAGE);
+});
