ef4fd37d7fd12d7735c03b1f7e93cd86e8f51e4b
photoby64 <photoby64@naver.com>
2025-01-08 14:23:48 +0900

    learn.react
    -25.01.08 오후 수업(1)

--- src/app.ts
+++ src/app.ts
@@ -14,16 +14,30 @@
 // ✅ Node.js의 fsPromises.readFile() API 함수 사용
 // ✅ Express.js의 라우팅 핸들러(entry) 분리
 // ✅ Express.js 앱의 미들웨어(Middleware) - greetingMessage.ts
-// ✅ 정적 자산 서비스 설정
+// ✅ GET 요청/응답 처리
+// ✅ https://www.typescriptlang.org/ko/docs/handbook/utility-types.html#picktype-keys
+// ✅ 
 // --------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------
+// TypeScript + Express.js를 활용해 간단한 API 서버 구성
+// --------------------------------------------------------------------------
+
 import 'dotenv/config';
 import express from 'express';
 import type { Express } from 'express';
+import { Request, response } from 'express';
+import type User from './types/user';
+import { resolve } from 'node:path';
 import entryHandler from './handlers/entry';
 import greetingMessage from './middlewares/greetingMessage';
-import { resolve } from 'node:path';
+import { request } from 'node:http';
+import { RequestUser } from './types/user';
+import { readFile,writeFile } from 'node:fs/promises';
 
-const app: Express = express();
+/* CONFIG. ------------------------------------------------------------------ */
 
 // for Windows Users
 const HOSTNAME = 'localhost';
@@ -31,16 +45,96 @@ const HOSTNAME = 'localhost';
 const PORT = Number(process.env.PORT) ?? 4000;
 const MESSAGE = `웹 서버 구동 : http://${HOSTNAME}:${PORT}`;
 
+/* Application -------------------------------------------------------------- */
+
+const app: Express = express();
 
 /* Middleware --------------------------------------------------------------- */
 
 app.use(greetingMessage);
-// app.use(express.static('public'));
 app.use(express.static(resolve(__dirname, '../public')));
+app.use(express.json());
 
 /* Routing ------------------------------------------------------------------ */
+//
+// 라우팅은 특정 엔드포인트에 대한 클라이언트 요청에 애플리케이션이 어떻게 응답할지 결정하는 것을 말하며,
+// 이는 URI(또는 경로)와 특정 HTTP 요청 메서드(GET, POST, PUT, PATCH, DELETE 등)입니다.
+// 각 경로에는 하나 이상의 핸들러 함수가 있을 수 있으며, 이 함수는 경로가 일치할 때 실행됩니다.
+//
+// --------------------------------------------------------------------------
 
-app.get('/', entryHandler);
+// GET
+// app.get('/', entryHandler);
+
+// POST
+// app.post('/', (request, response) => {
+//   // 클라이언트 요청 URL
+//   console.log(request.url);
+
+//   // 서버 -> 클라이언트 응답
+//   response.status(201 /* Created */).send({
+//     message: 'POST 요청이 홈페이지로부터 주어졌습니다.',
+//   });
+// });
+
+/* Users API ---------------------------------------------------------------- */
+
+const dummyUser: User = {
+  id: 1,
+  name: '박하신',
+  gender: '여성',
+  age: 25,
+};
+
+const dummyUserList: User[] = [dummyUser];
+
+// CREATE ----------------------------------------------------------------------
+
+// `POST /api/users`
+
+app.post('/api/users', 
+  async (request: Request<{}, {}, RequestUser>, response) => {
+  // 클라이언트 요청(JSON)
+  // console.log(request.body);
+
+  // 서버에서 프로그래밍
+  const usersString = await readFile(resolve(__dirname, './data/user.json'),{
+    encoding:'utf-8'
+  });
+
+  const usersJSON : User[] = JSON.parse(usersString);
+
+  console.log(usersJSON);
+
+  // 클라이언트에 응답
+  response.status(201).json({});
+
+  // 실패한 경우
+
+});
+
+
+// READ ------------------------------------------------------------------------
+
+// `GET /api/users`
+app.get('/api/users', (request, response) => {
+  // Response (to Client)
+  response.status(200).json(dummyUserList);
+});
+
+// `GET /api/users/:id`
+
+// UPDATE ---------------------------------------------------------------------
+
+// `PUT /api/users/:id`
+
+// `PATCH /api/users/:id`
+
+// DELETE ---------------------------------------------------------------------
+
+// `DELETE /api/users/:id`
+
+/* Listening ---------------------------------------------------------------- */
 
 app.listen(PORT, HOSTNAME, () => {
   console.log(MESSAGE);
---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+interface User {
+  id:number,
+  name:string,
+  gender:string,
+  age:number,
+
+}
+
+// export type RequestUser = Pick<User, 'age' | 'gender' | 'name'>;
+export type RequestUser = Omit<User, 'id'>;
+
+export default User
